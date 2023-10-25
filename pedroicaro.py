75404f5d497e9ca8204b638b30387d5625d7a04b
icarop16 <147431562+icarop16@users.noreply.github.com>
2023-10-25 11:47:39 -0300

    Add files via upload

---
+++ pedroicaro.py
@@ -0,0 +1,6 @@
+#Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+contador = 0
+while contador < 50:
+    contador +=2
+    print (f"{contador}")
+   
